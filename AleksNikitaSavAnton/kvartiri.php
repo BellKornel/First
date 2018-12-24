d5735325f9856521c7dd375dcf147e06a93cbd44
NikitaAnton <46017758+NikitaAnton@users.noreply.github.com>
2018-12-24 16:49:31 +0900

    Update kvartiri.php

--- AleksNikitaSavAnton/kvartiri.php
+++ AleksNikitaSavAnton/kvartiri.php
@@ -50,7 +50,7 @@
           		  	<br>
           		  	etazh: <input type="text" name="etazh">
           		  	<br>
-          		  	kratk opsi: <input type="text" name="kratk">
+          		  	kratk opisanie: <input type="text" name="kratk">
           		  	<br>
 				4elovek: 
 					<select name="cic">
